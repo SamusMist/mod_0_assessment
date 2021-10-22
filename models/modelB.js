ae687477cc9e859a989c6c4bc7817a5026e55ce6
Sam Mistele <smmstl0@gmail.com>
2021-10-22 09:43:23 -0600

    Add vehicle object

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+// Object model for a vehicle
+var vehicle = {
+  make: 'Toyota',
+  model: '4runner',
+  mileage: 25500,
+  year: 2018,
+  manualTransmission: false,
+  hasAccident: true,
+  addOns: ['snow tires', 'roof rack', 'mud guards', 'window tint'],
+  maintenanceRecord:['11/21/2018', '4/15/2019', '9/14/2019', '3/22/2020']
