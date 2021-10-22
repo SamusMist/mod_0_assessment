5eb588835da8794b52c9643fe1cc1d302e9b9341
Sam Mistele <smmstl0@gmail.com>
2021-10-22 09:42:25 -0600

    Add airport object

---
+++ models/modelA.js
@@ -0,0 +1,11 @@
+//Object model for an airport
+var airport = {
+  name: 'General Mitchell',
+  city: 'Milwaukee',
+  numOfGates: 3,
+  numOfAirlines: 5,
+  hasTsaPreCheck: true,
+  hasShuttle: true,
+  airlines: ['Delta', 'Southwest', 'Jetblue', 'Air Canada', 'Frontier'],
+  deltaFlightsToDenverToday: [0300, 0800, 1100, 1400, 1700, 2200]
+};
