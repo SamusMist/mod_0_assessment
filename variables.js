1934f3b01047825b87cba230d0051f9502bd35ad
Sam Mistele <smmstl0@gmail.com>
2021-10-22 09:22:47 -0600

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,17 @@
+var famousPerson = "David Attenborough";
+
+var isActive = true;
+
+famousPerson = "Larry David";
+
+var num3 = 77;
+
+var num9 = 99;
+
+var sum = num3 + num9;
+
+var animals = ['cat', 'dog', 'catdog'];
+
+animals.shift();
+
+famousPerson.length();
